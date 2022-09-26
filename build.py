123024072469a8abc0029cc4fd696405f47f3b29
Tom <elunico@users.noreply.github.com>
2022-09-26 17:40:07 -0400

    Initial commit

---
+++ build.py
@@ -0,0 +1,121 @@
+from curses import meta
+from datetime import datetime
+import json
+from pkgutil import extend_path
+from pydoc import classname
+import markdown
+import os
+import urllib
+import os.path
+import re
+import msgpack
+
+html_dir = 'public'
+
+
+def serialize(metadata, folder, file_prefix):
+    with open(os.path.join(folder, '{}.json'.format(file_prefix)), 'w') as f:
+        json.dump(metadata, f)
+
+
+def unserialize(folder, file_prefix):
+    with open(os.path.join(folder, '{}.json'.format(file_prefix))) as f:
+        return json.load(f)
+
+
+def linkify(title):
+    return urllib.parse.quote(title)
+
+
+def titlify(filename):
+    return '.'.join(filename.rsplit('.')[:-1]).title()
+
+
+def tag_spans(iterator, className=''):
+    return '\n'.join('<span class="{}"><a href="search/{}">{}</a></span>'.format(className, urllib.parse.quote(i), i) for i in iterator)
+
+
+def style(html_template):
+    with open(os.path.join('private', 'stylesheet.css')) as f:
+        return html_template.replace('@css-content {}', f.read())
+
+
+def index_entry(filename, meta):
+    return '''
+    <div class="post">
+    <div class="blog-title"> <a href="{}">{}</a></div>
+    <div class="summary">{}</div>
+    <div class="footer tags">Tags: {}</div>
+    <div class="footer published">Published: <span class="pub-date">{}</span></div>
+    </div>
+    <hr>
+    '''.format(linkify(filename), titlify(filename), meta['summary'], tag_spans(meta['tags'], className='tag'),
+               meta['date'])
+
+
+def html_name(md_filename):
+    *filename, ext = md_filename.rsplit('.')
+    name = '.'.join(filename)
+    return '{}.html'.format(name)
+
+
+def strip_file_metadata(path):
+    file_meta = {'tags': [], 'data': '', 'summary': 'No Summary Provided!'}
+    content = ''
+    with open(path) as f:
+        for line in f:
+            if line.startswith('%tags'):
+                try:
+                    file_meta['tags'] = [i for i in re.split(r',\s+', line[5:]) if i]
+                except (IndexError) as e:
+                    raise ValueError('Invalid tag metadata') from e
+            elif line.startswith('%date'):
+                try:
+                    # validate date by parsing
+                    file_meta['date'] = datetime.fromisoformat(line[5:]).isoformat()
+                except (IndexError, ValueError, TypeError) as e:
+                    raise ValueError('Invalid date metadata') from e
+            elif line.startswith('%summary'):
+                try:
+                    file_meta['summary'] = line[len('%summary'):]
+                except IndexError as e:
+                    raise ValueError("Invalid summary metadata") from e
+            else:
+                content += line + '\n'
+
+    if 'date' not in file_meta:
+        s = os.stat(path)
+        file_meta['date'] = datetime.fromtimestamp(s.st_birthtime).isoformat()
+
+    return file_meta, content
+
+
+def write_index(content):
+    with open(os.path.join('private', 'index.html.template')) as f:
+        template = f.read()
+
+    with open(os.path.join(html_dir, 'index.html'), 'w') as f:
+        f.write(style(template).replace('%{{content}}', content))
+
+
+def main():
+    index_content = ''
+    blog_meta = {}
+    for file in os.listdir('source'):
+        print("Building file {}".format(file))
+        path = os.path.join('source', file)
+        blog_meta[file], file_content = strip_file_metadata(path)
+        file_content = re.sub(r'\n([^\n])', r'\1', file_content)
+        index_content += index_entry(html_name(file), blog_meta[file])
+        html = markdown.markdown(file_content, extensions=['fenced_code', 'codehilite', 'toc'])
+        with (open(os.path.join(html_dir, html_name(file)), 'w') as f,
+                open(os.path.join('private', 'article.html.template')) as g):
+            f.write(style(g.read()).replace("%{{content}}", html))
+
+    write_index(index_content)
+
+    serialize(blog_meta, 'public', 'metadata')
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
---
+++ source/my first post.md
@@ -0,0 +1,40 @@
+%tags first post, topic start, explanation, meta
+%summary The first post explaining my motivation for starting and a blog
+
+
+# My First Post 😃
+
+## Contents
+
+[TOC]
+
+This is my first post on what I am calling my "blog". This blog is a little different than most
+people's blogs—at least when they intend to start a blog for blogging. I am starting this blog
+not because I have a lot to say nor because I was sponsored by SquareSpace, but rather because
+I wanted to try building a Markdown to HTML build system for a website. I intend to publish to this
+blog as if it were my real blog, but I will be doing everything through my own work. All blog
+entries will start like as Markdown files and will be translated. Metadata will be parsed and
+(eventually) searchable (currently clicking a tag brings you a 404), and the index will be rendered
+from the list of entries during the build process.
+
+## Why am I rolling my own blog?
+It sounded fun. I thought it would be interesting to put together a system like this
+
+## Why don't I just use a well-established hosting platform?
+I don't really have the time to commit to maintaing a blog and paying for a professional service for something
+I don't think I can commit to regularly seems silly. Plus writing my own is way more fun so I get to have a blog and a fun project.
+
+## What about logging in, how you will draft new posts?
+If I wanted to add authentication I would **not** choose to do that myself, but since I am the only contributor to this
+blog and I am doing it mostly for fun on the side, I will simply manage entries locally on my computer. I have no
+need for authentication on my own personal blog, so why bother.
+
+## Can other people use this build system?
+My blog is hosted on Github pages (for now) and the repository is (by necessity) public, however
+I extremely do not recommend using it since—as mentioned previously—this is mostly a thing I am doing for fun
+and not to be the next WordPress. If you want a blog, just use WordPress; if you want to have fun, try
+rolling your own, but I wouldn't recommend using mine.
+
+## What will you write about?
+The build process probably. Mac OS X, OS X, and macOS stuff. Programming and technology in general. Probably a little
+about fonts and typography also.
