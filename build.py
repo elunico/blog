0819f3a5acd9d4752c0bcba1503ab0afebb1b32b
Tom <elunico@users.noreply.github.com>
2022-09-26 19:31:51 -0400

    Split up python for organization

--- build.py
+++ build.py
@@ -10,48 +10,11 @@ import urllib
 import os.path
 import re
 
+from python.util import *
+from python.html import *
+
 
 html_dir = 'public'
-
-
-def serialize(metadata, folder, file_prefix):
-    with open(os.path.join(folder, '{}.json'.format(file_prefix)), 'w') as f:
-        json.dump(metadata, f)
-
-
-def unserialize(folder, file_prefix):
-    with open(os.path.join(folder, '{}.json'.format(file_prefix))) as f:
-        return json.load(f)
-
-
-def linkify(title):
-    return urllib.parse.quote(title)
-
-
-def titlify(filename):
-    return '.'.join(filename.rsplit('.')[:-1]).title()
-
-
-def tag_spans(iterator, className=''):
-    return '\n'.join('<span class="{}"><a href="search/{}">{}</a></span>'.format(className, urllib.parse.quote(i), i) for i in iterator)
-
-
-def style(html_template):
-    with open(os.path.join('private', 'stylesheet.css')) as f:
-        return html_template.replace('@css-content {}', f.read())
-
-
-def index_entry(filename, meta):
-    return '''
-    <div class="post">
-    <div class="blog-title"> <a href="{}">{}</a></div>
-    <div class="summary">{}</div>
-    <div class="footer tags">Tags: {}</div>
-    <div class="footer published">Published: <span class="pub-date">{}</span></div>
-    </div>
-    <hr>
-    '''.format(linkify(filename), titlify(filename), meta['summary'], tag_spans(meta['tags'], className='tag'),
-               meta['date'])
 
 
 def html_name(md_filename):
---
+++ python/util.py
@@ -0,0 +1,12 @@
+import json
+import os.path
+
+
+def serialize(metadata, folder, file_prefix):
+    with open(os.path.join( folder, '{}.json'.format(file_prefix)), 'w') as f:
+        json.dump(metadata, f)
+
+
+def unserialize(folder, file_prefix):
+    with open(os.path.join( folder, '{}.json'.format(file_prefix))) as f:
+        return json.load(f)
