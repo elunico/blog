881de10161caa10e39bde101cb1315f6560895cd
Tom <elunico@users.noreply.github.com>
2022-10-03 23:26:38 -0400

    continuing style tweaks

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,4 @@ python build.py &&
   git add . && 
   git commit -m "$MESSAGE" && 
   git push origin main && 
-  git subtree split --rejoin --prefix public --ignore-joins
   git subtree push --prefix public origin gh-pages
